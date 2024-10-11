a60c035f0f66106da6ef961edd7d14df4ee33f6e
Whuss88 <williamhussey95@gmail.com>
2024-10-11 10:26:01 -0400

    added second funtion

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 
-const logPresent = () => {
-  return (`Im here`) }
+// const logPresent = () => {
+//   return (`Im here`) }
 
-logPresent ();
-let result = logPresent()
-console.log(result)
+// logPresent ();
+// let result = logPresent()
+// console.log(result)
+
+const logPresent = () => {
+  console.log (`im here`);
+}
+
+logPresent();
